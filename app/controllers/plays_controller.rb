b9e16164a05ea96026e897516da3aa4b89e195e5
wawer77 <tomek.06@wp.pl>
2018-08-23 01:25:12 +0200

    Add edit and delete methods

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -6,7 +6,6 @@ def index
 end
 
 def show
-
 end
 
 def new
@@ -20,6 +19,22 @@ def create
   else
     render 'new'
   end
+end
+
+def edit
+end
+
+def update
+  if @play.update(play_params)
+    redirect_to play_path(@play)
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @play.destroy
+  redirect_to root_path
 end
 
 private
